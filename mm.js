3c094bdb7d74b9c56d073f0b239cc31126646589
MOHAMED KAREM <164479904+HAPPYXXSad@users.noreply.github.com>
2024-03-27 12:56:15 +0200

    Add files via upload

---
+++ mm.js
@@ -0,0 +1,30 @@
+    let menu = document.querySelector('#menu-bars') ;
+    let navbar = document.querySelector('.navbar') ;
+   
+    Window.onscroll = () =>{
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    }
+
+    document.querySelector('#search-icon').onclick  = () =>{
+        document.querySelector('#search-form').classList.toggle('active');
+    }
+
+    document.querySelector('#close').onclick = () =>{
+        document.querySelector('#search-form').classList.remove('active');
+    }
+
+    
+    var swiper = new Swiper(".home-slider", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        loop:true,
+      });
